4d81c2d8396db9505f1d3e0d5278abaad22b1e08
Gregory Saumier-Finch <gregory@culturecreates.com>
2021-11-11 07:27:00 -0500

    Added limit param

--- js/artsdataApi.js
+++ js/artsdataApi.js
@@ -1,8 +1,9 @@
 const apiKey = '' ;
 const format = 'json' ;
-const source = 'http://kg.artsdata.ca/culture-creates/footlight/osm-ca' ; 
+const source = 'http://kg.artsdata.ca/culture-creates/footlight/spec-qc-ca' ; 
 const frame = 'event_bn';
+const limit = 10;
 const baseUrl = 'http://api.artsdata.ca/events' ;
-const artsdataApiUrl = `${baseUrl}?format=${format}&source=${source}&frame=${frame}` ;
+const artsdataApiUrl = `${baseUrl}?format=${format}&source=${source}&frame=${frame}&limit=${limit}` ;
 
 export { artsdataApiUrl } ;
