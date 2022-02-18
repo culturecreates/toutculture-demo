f35a83e496e898a4060bb4cc3fa5a04a241b4c90
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-02-18 08:46:25 -0500

    points to api.artsdata.ca

--- js/artsdataApi.js
+++ js/artsdataApi.js
@@ -1,7 +1,7 @@
-const apiKey = 'sandbox' ;
-const format = 'json' ;  // json|jsonld
+const apiKey = 'sandbox' ;  // limited calls. Contact info@culturecreates.com for your apiKey.
+const format = 'json' ;  // json or jsonld
 const source = 'http://kg.artsdata.ca/culture-creates/footlight/spec-qc-ca' ; // see list at https://s.zazuko.com/7w6bJ
-const baseUrl = 'https://artsdata-api-staging.herokuapp.com' ;
+const baseUrl = 'https://api.artsdata.ca' ;
 const endPoint = `${baseUrl}/events`
 const artsdataApiUrl = `${endPoint}?apiKey=${apiKey}&format=${format}&source=${source}` ;
 
