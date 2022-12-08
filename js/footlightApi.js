85bff8ce10090a6f3f691938312a8c8085dbef5c
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-12-07 20:43:50 -0500

    added places

--- js/footlightApi.js
+++ js/footlightApi.js
@@ -1,5 +1,6 @@
 const baseUrl = "https://staging.api.footlight.io";
-const endPoint = `${baseUrl}/calendars/tout-culture/events`;
-const footlightApiUrl = `${endPoint}?page=1&limit=100`;
+const endPoint = `${baseUrl}/calendars/tout-culture/`;
+const footlightApiUrl = `${endPoint}events?page=1&limit=100`;
+const footlightApiPlacesUrl = `${endPoint}places?page=1&limit=100`;
 
-export { footlightApiUrl };
+export  { footlightApiUrl, footlightApiPlacesUrl};
---
+++ js/places.js
@@ -0,0 +1,17 @@
+import { footlightApiPlacesUrl } from "./footlightApi.js";
+
+
+places();
+
+async function places() {
+  const res = await fetch(footlightApiPlacesUrl);
+  const json = await res.json();
+  console.log(json);
+  const main = document.querySelector("main");
+  json.data.forEach((place) => {
+    
+    const el = document.createElement("div" );
+    el.innerHTML = `${place.name.fr} <br>Geo: ${place.geo.latitude}, ${place.geo.longitude}`;
+    main.appendChild(el);
+  });
+}
