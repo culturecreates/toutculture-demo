42863a3158ef970da060c88b6ccb4c7884713437
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-12-07 22:29:21 -0500

    spacing

--- js/footlightApi.js
+++ js/footlightApi.js
@@ -4,4 +4,4 @@ const footlightApiUrl = `${endPoint}events?page=1&limit=100`;
 const footlightApiPlacesUrl = `${endPoint}places?page=1&limit=100&include-event-count=true`;
 
 
-export  { footlightApiUrl, footlightApiPlacesUrl};
+export  { footlightApiUrl, footlightApiPlacesUrl };
