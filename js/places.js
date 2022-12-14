6be712e7f7f230bf1e2f3d24bb0a7ba2c2570c7c
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-12-14 10:16:26 -0500

    added count to places

--- js/places.js
+++ js/places.js
@@ -7,16 +7,24 @@ async function places() {
   const res = await fetch(footlightApiPlacesUrl);
   const json = await res.json();
   console.log(json);
-  const main = document.querySelector("main");
+  const main = document.querySelector("ol");
   json.data.forEach((place) => {
     
     const el = document.createElement("div" );
+    el.innerHTML += `<li><h2>${place.name.fr}</h2>`;
+    el.innerHTML += `${place.address.streetAddress.fr}`;
     if (place.additionalType != undefined) {
-      el.innerHTML = `<h2>${place.name.fr}</h2> <br> ${displayList(place.additionalType)} <br>Events: ${place.eventCount}<br>Geo: ${place.geo.latitude}, ${place.geo.longitude}`;
+      el.innerHTML += `<br>Type: ${displayList(place.additionalType)}`;
+    } 
+    if (place.containedInPlace != undefined) {
+      el.innerHTML += `<br>ContainedInPlace: ${place.containedInPlace.name.fr}`;
+    } 
+    el.innerHTML += `<br>Event count: ${place.eventCount}`;
 
-    } else {
-      el.innerHTML = `<h2>${place.name.fr}</h2>  <br>Events: ${place.eventCount}<br>Geo: ${place.geo.latitude}, ${place.geo.longitude}`;
-    }
+
+
+    el.innerHTML += `<br>Geo: ${place.geo.latitude}, ${place.geo.longitude}`;
+    el.innerHTML += `</li>`
     main.appendChild(el);
   });
 }
--- js/utils/bilingual-array.js
+++ js/utils/bilingual-array.js
@@ -1,7 +1,7 @@
 export const displayList = (list) => {
   var html = "";
   list.forEach((item) => {
-    html += `${item.name.fr || item.name.en || ""}<br>`;
+    html += `${item.name.fr || item.name.en || ""}   `;
   });
   return html;
 };
