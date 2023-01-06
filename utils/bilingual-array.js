a8abda4cfcf25b59a389938bc9eaffc21f6fa728
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-01-06 12:19:19 -0500

    fix bilingual array

--- utils/bilingual-array.js
+++ utils/bilingual-array.js
@@ -2,7 +2,9 @@ export const displayList = (list) => {
   var html = "";
   if (list != undefined) {
     list.forEach((item) => {
+      if (item.name != undefined) {
       html += `${item.name.fr || item.name.en || ""}   `;
+    }
     });
   
   }
