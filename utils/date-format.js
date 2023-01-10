b6331b1ad3619bdf107980e0f4c761cdf7d10a60
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-01-09 22:41:38 -0500

    Added prefix to show date time or date

--- utils/date-format.js
+++ utils/date-format.js
@@ -1,15 +1,28 @@
 
   export const dateFormat = (event) => {
     var html = "";
-    html += new Date(event.startDate).toLocaleString("fr-FR", {
-      dateStyle: "medium",
-      timeStyle: "short",
-      hour12: false,
-      timeZone: "EST"
-    });
+    if (event.startDate) {
+      html += `DATE: ${event.startDate}`;
+    }
+
+    if (event.startDateTime) {
+      html += "DATE-TIME: ";
+      html += new Date(event.startDateTime).toLocaleString("fr-FR", {
+        dateStyle: "medium",
+        timeStyle: "short",
+        hour12: false,
+        timeZone: "EST"
+      });
+    }
+
     if (event.endDate) {
       html +=
-        "<br>--> " +
+        `<br>--> DATE: ${event.endDate}`
+    }
+
+    if (event.endDateTime) {
+      html +=
+        "<br>--> DATE-TIME: " +
           new Date(event.endDate).toLocaleString("fr-FR", {
             dateStyle: "medium",
             timeStyle: "short",
@@ -17,5 +30,7 @@
             timeZone: "EST"
           }) || "";
     }
+
+
     return html;
   }
