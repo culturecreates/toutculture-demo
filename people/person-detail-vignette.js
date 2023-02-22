49277638904ecc13f32146d8c83b2f73baff391d
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-02-22 14:45:30 -0500

    safe navigate person

--- people/person-detail-vignette.js
+++ people/person-detail-vignette.js
@@ -6,13 +6,13 @@ class PersonDetailVig extends HTMLElement {
   set details(person) {
     this.innerHTML = `
     <div>
-    <div class="box"><img src="${person.image.thumbnail || ""}"></div>
+    <div class="box"><img src="${person.image?.thumbnail || ""}"></div>
 
-    <h3> ${person.name.fr || person.name.en} </h3>
+    <h3> ${person.name?.fr || person.name?.en} </h3>
 
     <p> <b>Type:</b> ${concepts(person.additionalType)} </p>
  
-    <p> <b>Description:</b> ${person.description.fr || person.description.en }
+    <p> <b>Description:</b> ${person.description?.fr || person.description?.en }
     </div>`;
   }
 }
