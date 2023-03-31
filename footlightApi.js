d4a3060b29296fc975e9963f0868e7a95a349bf0
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-03-30 20:35:47 -0400

    list only org members of outaouias

--- footlightApi.js
+++ footlightApi.js
@@ -2,7 +2,7 @@ const baseUrl = "https://staging.api.footlight.io";
 const endPoint = `${baseUrl}/calendars/tout-culture/`;
 const footlightApiUrl = `${endPoint}events?page=1&limit=100`;
 const footlightApiPlacesUrl = `${endPoint}places?sort=name.fr&page=1&limit=100&include-event-count=true`;
-const footlightApiOrganizationsUrl = `${endPoint}organizations?sort=name.fr&page=1&limit=100`;
+const footlightApiOrganizationsUrl = `${endPoint}organizations?sort=name.fr&page=1&limit=100&concept=635fa2ab7086d70059aca2bc`;
 const footlightApiPeopleUrl = `${endPoint}people?sort=name.en&page=1&limit=100`;
 const footlightApiEventDetails = `${baseUrl}/events`;
 const footlightApiPersonDetails = `${baseUrl}/people`;
