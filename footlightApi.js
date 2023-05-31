91bcc9af32faf48036266818536519e9bcc63c46
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-05-31 10:08:27 -0400

    limit number of events

--- footlightApi.js
+++ footlightApi.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://staging.api.footlight.io";
 const endPoint = `${baseUrl}/calendars/tout-culture/`;
-const footlightApiUrl = `${endPoint}events?page=1&limit=100`;
+const footlightApiUrl = `${endPoint}events?page=1`;
 const footlightApiPlacesUrl = `${endPoint}places?sort=name.fr&page=1&limit=100&include-event-count=true`;
 const footlightApiOrganizationsUrl = `${endPoint}organizations?sort=name.fr&page=1&limit=100&concept=635fa2ab7086d70059aca2bc`;
 const footlightApiPeopleUrl = `${endPoint}people?sort=name.en&page=1&limit=100`;
