3354a8fd66f1de9c3e8b0a2e8a211d37ff7c30ac
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-07-12 08:32:23 -0400

    added workflow to switch search widget to point to production search widget

--- events/event-edit-link.js
+++ events/event-edit-link.js
@@ -1,8 +1,8 @@
+import { footlightApiUrl } from  "../footlightApi.js";
+
 export const generateEnvironmentEditLink = (event) => {
-  if (!event.image) {
-    return "";
-  }
-  if (event.image.thumbnail.startsWith("https://cdn.")) {
+
+  if (footlightApiUrl.startsWith("https://api.footlight.io") ) {
     return `edit in <a href="https://oldcms.footlight.io/admin/add-event/?id=${event.id}">oldcms<a></a>`;
   } else {
     return `edit in <a href="http://staging.oldcms.footlight.io/admin/add-event/?id=${event.id}">oldcms<a></a>`;
