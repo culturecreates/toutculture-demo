f37124f244fa0ca54a8872e26d77d5bc770e0dce
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-10-20 08:28:29 -0400

    edit events in new cms

--- events/event-edit-link.js
+++ events/event-edit-link.js
@@ -3,9 +3,9 @@ import { footlightApiUrl } from  "../footlightApi.js";
 export const generateEnvironmentEditLink = (event) => {
 
   if (footlightApiUrl.startsWith("https://api.footlight.io") ) {
-    return `edit in <a href="https://oldcms.footlight.io/admin/add-event/?id=${event.id}">oldcms<a></a>`;
+    return `edit in <a href="https://cms.footlight.io/dashboard/6308ef4a7f771f00431d939a/events/add-event/${event.id}">new cms</a>`;
   } else {
-    return `edit in <a href="http://staging.oldcms.footlight.io/admin/add-event/?id=${event.id}">oldcms<a></a>`;
+    return `edit in <a href="https://staging.cms.footlight.io/dashboard/6308ef4a7f771f00431d939a/events/add-event/${event.id}">new cms</a>`;
   }
  
 }
